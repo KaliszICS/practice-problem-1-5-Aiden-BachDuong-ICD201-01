f552164b96dc8be8c6f84d1919997fac480fce7b
Aiden-BachDuong-ICD201-01 <aiden.bachduong@student.tdsb.on.ca>
2024-09-23 14:49:19 +0000

    jdwankdan

--- PP1_5.py
+++ PP1_5.py
@@ -1,17 +1,44 @@
 
 def q1():
+  input("Input an interger: ")
+  num = int(num) 
+  print(num + 3)
+
   #Write Assignment code here
 
 def q2():
+  input("Input a number: ")
+  num = int(num)
+  print(str, end='4')
+  str = (float + "2")
+  print(num + str)
+
   #Write Assignment code here
 
 def q3():
+  input("Input a radius: ")
+  radius = (input)
+  float = (radius)
+  pie = ("3.14")
+  print("(float)x 2 x pie")
+
   #Write Assignment code here
 
 def q4():
+  input("Input a number: ")
+  num = input(num)
+  num = float(num) 
+  num = str(num)
+  print(num x "12" // "10")
+
   #Write Assignment code here
 
 def q5():
+  input("Input an interger: ")
+  num = int(num)
+  int = (num + "5")
+  {var} = (int)
+  print("Your number + 5 is {var}")
   #Write Assignment code here
 
 #Comment this code out when running tests
